a3ee699926ce455c216d25d7045b970aba2002c0
luismedinaeng <malf95@hotmail.com>
2020-06-11 23:43:17 -0500

    [Add]CRUD operations to users with the API

---
+++ api/v1/views/users.py
@@ -0,0 +1,87 @@
+#!/usr/bin/python3
+""" Stores API """
+
+from api.v1.app import mongo
+from api.v1.views import app_views
+from bson.objectid import ObjectId
+from flask import abort, jsonify, make_response, request
+
+def validate_user(data_user):
+    required = ['name', 'email', 'passwd', 'type']
+    for field in required:
+        if field not in data_user:
+            return False
+    return True
+
+def created_user(user_email):
+    user = mongo.db.users.find_one({"email": user_email})
+    print(user, user_email)
+    return True if user else False
+
+@app_views.route('/users', methods=['GET'])
+def get_users():
+    users_list = mongo.db.users.find()
+    user = []
+    for one_user in users_list:
+        one_user['_id'] = str(one_user['_id'])
+        user.append(one_user)
+    return make_response(jsonify(user), 200)
+
+@app_views.route('/users/<user_id>', methods=['GET'])
+def get_one_user(user_id):
+    user = mongo.db.users.find_one({"_id": ObjectId(user_id)})
+    if not user:
+        abort(404)
+    user['_id'] = str(user['_id'])
+    return make_response(jsonify(user), 200)
+
+@app_views.route('/users/<user_id>/stores', methods=['GET'])
+def get_one_user_stores(user_id):
+    user = mongo.db.users.find_one({"_id": ObjectId(user_id)})
+    if not user or user['type'] != 'owner':
+        abort(404)
+    stores_db = mongo.db.stores.find()
+    stores_user = []
+    for store in stores_db:
+        if str(user['_id']) == store['owner_id']:
+            store['_id'] = str(store['_id'])
+            stores_user.append(store)
+    return make_response(jsonify(stores_user), 200)
+
+@app_views.route('/users', methods=['POST'])
+def post_user():
+    data = request.get_json()
+    if not data:
+        abort(400, description="Not a JSON")
+    if not validate_user(data):
+        abort(400, description="Bad JSON: Some required field is missing")
+    if created_user(data['email']):
+        abort(409, description="Email already used")
+    user_id = str(mongo.db.users.insert(data))
+    user = mongo.db.users.find_one({"_id": ObjectId(user_id)})
+    user['_id'] = str(user['_id'])
+    return make_response(jsonify(user), 200)
+
+@app_views.route('/users/<user_id>', methods=['PUT'])
+def update_user(user_id):
+    data = request.get_json()
+    if not data:
+        abort(400, description="Not a JSON")
+    user = mongo.db.users.find_one({"_id": ObjectId(user_id)})
+    if not user:
+        abort(404)
+    for key, item in data.items():
+        mongo.db.users.update_one({"_id": ObjectId(user_id)},
+                                       {'$set': {key: item}})
+    user = mongo.db.users.find_one({"_id": ObjectId(user_id)})
+    user['_id'] = str(user['_id'])
+    return make_response(jsonify(user), 200)
+
+@app_views.route('/users/<user_id>', methods=['DELETE'])
+def delete_user(user_id):
+    user = mongo.db.users.find_one({"_id": ObjectId(user_id)})
+    if not user:
+        abort(404)
+    mongo.db.users.remove({'_id': ObjectId(user_id)})
+    user['_id'] = str(user['_id'])
+    return make_response(jsonify(user), 200)
