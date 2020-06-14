ebb9b380324ff82d2fb78091b8ccaa7c1349d7a0
luismedinaeng <malf95@hotmail.com>
2020-06-13 19:13:02 -0500

    [Update]" for '

--- api/v1/views/users.py
+++ api/v1/views/users.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-""" Stores API """
+''' Stores API '''
 
 from api.v1.app import mongo
 from api.v1.views import app_views
@@ -14,7 +14,7 @@ def validate_user(data_user):
     return True
 
 def created_user(user_email):
-    user = mongo.db.users.find_one({"email": user_email})
+    user = mongo.db.users.find_one({'email': user_email})
     print(user, user_email)
     return True if user else False
 
@@ -29,7 +29,7 @@ def get_users():
 
 @app_views.route('/users/<user_id>', methods=['GET'])
 def get_user(user_id):
-    user = mongo.db.users.find_one({"_id": ObjectId(user_id)})
+    user = mongo.db.users.find_one({'_id': ObjectId(user_id)})
     if not user:
         abort(404)
     user['_id'] = str(user['_id'])
@@ -39,13 +39,13 @@ def get_user(user_id):
 def post_user():
     data = request.get_json()
     if not data:
-        abort(400, description="Not a JSON")
+        abort(400, description='Not a JSON')
     if not validate_user(data):
-        abort(400, description="Bad JSON: Some required field is missing")
+        abort(400, description='Bad JSON: Some required field is missing')
     if created_user(data['email']):
-        abort(409, description="Email already used")
+        abort(409, description='Email already used')
     user_id = mongo.db.users.insert(data)
-    user = mongo.db.users.find_one({"_id": user_id})
+    user = mongo.db.users.find_one({'_id': user_id})
     user['_id'] = str(user['_id'])
     return make_response(jsonify(user), 200)
 
@@ -53,24 +53,24 @@ def post_user():
 def update_user(user_id):
     data = request.get_json()
     if not data:
-        abort(400, description="Not a JSON")
-    user = mongo.db.users.find_one({"_id": ObjectId(user_id)})
+        abort(400, description='Not a JSON')
+    user = mongo.db.users.find_one({'_id': ObjectId(user_id)})
     if not user:
         abort(404)
     if 'email' in data:
         data_email = data['email']
         if created_user(data_email) and user['email'] != data_email:
-            abort(409, description="Email already used")
+            abort(409, description='Email already used')
     for key, item in data.items():
-        mongo.db.users.update_one({"_id": ObjectId(user_id)},
+        mongo.db.users.update_one({'_id': ObjectId(user_id)},
                                        {'$set': {key: item}})
-    user = mongo.db.users.find_one({"_id": ObjectId(user_id)})
+    user = mongo.db.users.find_one({'_id': ObjectId(user_id)})
     user['_id'] = str(user['_id'])
     return make_response(jsonify(user), 200)
 
 @app_views.route('/users/<user_id>', methods=['DELETE'])
 def delete_user(user_id):
-    user = mongo.db.users.find_one({"_id": ObjectId(user_id)})
+    user = mongo.db.users.find_one({'_id': ObjectId(user_id)})
     if not user:
         abort(404)
     mongo.db.users.remove({'_id': ObjectId(user_id)})
