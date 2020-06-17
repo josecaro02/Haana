011a8cbaf7031f3ef9305315f1033aec5494405c
luismedinaeng <malf95@hotmail.com>
2020-06-16 23:34:15 -0500

    [Add]Token generation and sending to client

--- api/v1/auth/access.py
+++ api/v1/auth/access.py
@@ -6,6 +6,9 @@ from api.v1.auth import app_auth
 from bson.objectid import ObjectId
 from flask import abort, jsonify, make_response, request
 from models import mongo_db
+import datetime
+import jwt
+
 
 @app_auth.route('/', methods=['POST'])
 def user_login():
@@ -16,7 +19,12 @@ def user_login():
         abort(400, description="Not enough information")
     user = mongo_db.users.find_one({'email': data["email"]})
     if user and bcrypt.check_password_hash(user['passwd'], data['passwd']):
-        info = {'status': 'login succesful'}
+        payload = {'exp': datetime.datetime.utcnow() + datetime.timedelta(seconds=30),
+                   'iat': datetime.datetime.utcnow(),
+                   'email': user['email']}
+        token = jwt.encode(payload, 'haana', algorithm='HS256')
+        info = {'status': 'login succesful',
+                'auth_token': token.decode()}
         return make_response(jsonify(info), 200)
     else:
         abort(403, description="Invalid user or password")
