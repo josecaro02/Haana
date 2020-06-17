93a4d06133814fe562f95688fec7c8b80097bad4
luismedinaeng <malf95@hotmail.com>
2020-06-16 23:18:01 -0500

    [Add]Hashing user passwords to db

--- api/v1/views/users.py
+++ api/v1/views/users.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 ''' Stores API '''
 
+from api.v1.app import bcrypt
 from api.v1.views import app_views
 from bson.objectid import ObjectId
 from flask import abort, jsonify, make_response, request
@@ -15,7 +16,6 @@ def validate_user(data_user):
 
 def created_user(user_email):
     user = mongo_db.users.find_one({'email': user_email})
-    print(user, user_email)
     return True if user else False
 
 @app_views.route('/users', methods=['GET'])
@@ -24,6 +24,7 @@ def get_users():
     user = []
     for one_user in users_list:
         one_user['_id'] = str(one_user['_id'])
+        one_user.pop('passwd')
         user.append(one_user)
     return make_response(jsonify(user), 200)
 
@@ -33,6 +34,7 @@ def get_user(user_id):
     if not user:
         abort(404)
     user['_id'] = str(user['_id'])
+    user.pop('passwd')
     return make_response(jsonify(user), 200)
 
 @app_views.route('/users', methods=['POST'])
@@ -43,10 +45,12 @@ def post_user():
     if not validate_user(data):
         abort(400, description='Bad JSON: Some required field is missing')
     if created_user(data['email']):
-        abort(409, description='Email already used')
+        abort(409, description='Email already exists. Please Log in')
+    data['passwd'] = bcrypt.generate_password_hash(data['passwd']).decode()
     user_id = mongo_db.users.insert(data)
     user = mongo_db.users.find_one({'_id': user_id})
     user['_id'] = str(user['_id'])
+    user.pop('passwd')
     return make_response(jsonify(user), 200)
 
 @app_views.route('/users/<user_id>', methods=['PUT'])
@@ -62,10 +66,13 @@ def update_user(user_id):
         if created_user(data_email) and user['email'] != data_email:
             abort(409, description='Email already used')
     for key, item in data.items():
+        if key == 'passwd':
+            item = bcrypt.generate_password_hash(item).decode()
         mongo_db.users.update_one({'_id': ObjectId(user_id)},
                                        {'$set': {key: item}})
     user = mongo_db.users.find_one({'_id': ObjectId(user_id)})
     user['_id'] = str(user['_id'])
+    user.pop('passwd')
     return make_response(jsonify(user), 200)
 
 @app_views.route('/users/<user_id>', methods=['DELETE'])
@@ -75,4 +82,5 @@ def delete_user(user_id):
         abort(404)
     mongo_db.users.remove({'_id': ObjectId(user_id)})
     user['_id'] = str(user['_id'])
+    user.pop('passwd')
     return make_response(jsonify(user), 200)
